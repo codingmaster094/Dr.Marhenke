ea78fd8a1dfca3828c31475503fcd22364402638
dipak-trio-spire <dipak@trio-spire.com>
2025-06-04 14:39:30 +0530

    add leaflet

--- src/app/Map/page.js
+++ src/app/Map/page.js
@@ -1,16 +1,24 @@
 "use client";
+
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
+// Dynamic imports for React-Leaflet components
 const MapContainer = dynamic(
-  () => import("react-leaflet").then((m) => m.MapContainer)
+  () => import("react-leaflet").then((m) => m.MapContainer),
+  { ssr: false }
 );
 const TileLayer = dynamic(
-  () => import("react-leaflet").then((m) => m.TileLayer)
+  () => import("react-leaflet").then((m) => m.TileLayer),
+  { ssr: false }
 );
-const Marker = dynamic(() => import("react-leaflet").then((m) => m.Marker));
-const Popup = dynamic(() => import("react-leaflet").then((m) => m.Popup));
+const Marker = dynamic(() => import("react-leaflet").then((m) => m.Marker), {
+  ssr: false,
+});
+const Popup = dynamic(() => import("react-leaflet").then((m) => m.Popup), {
+  ssr: false,
+});
 
 const markers = [
   {
